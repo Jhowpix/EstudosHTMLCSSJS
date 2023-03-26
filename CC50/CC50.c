7ed614add6837ef7592ce330a6e341e245a4151f
Jhonatan de Oliveira <Jhonatandevbr@gmail.com>
2023-03-27 02:27:22 +0900

    iniciando CS50
    coletando mais conhecimento.

---
+++ CC50/CC50.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+
+   string answer = get_string("What's your name?\n");
+
+   string nome = get_string("Qual é seu nome?\n");
+
+}
---
+++ CS50Harvard/CS50.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h> // adicionando a bublioteca
+int main(void)
+{
+    printf("TITLE: Jhonatan de Oliveira Silva\n");
+
+    string answer = get_string("What's your name?\n");
+
+    printf("Yuor name is, %s\n", answer);
+
+
+}
