b6f43ef6d182eaf46618e6c7e90d0455350a41eb
Jhonatan de Oliveira <Jhonatandevbr@gmail.com>
2023-03-27 11:34:07 +0900

    Update CS50.c
    Segue a primeira aula.

--- CS50Harvard/CS50.c
+++ CS50Harvard/CS50.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#include <cs50.h> // adicionando a bublioteca
+#include <cs50.h> 
+
+    // Primeira aula CS50.
+
 int main(void)
 {
     printf("TITLE: Jhonatan de Oliveira Silva\n");
@@ -7,6 +10,46 @@ int main(void)
     string answer = get_string("What's your name?\n");
 
     printf("Yuor name is, %s\n", answer);
+
+// use help50 para detectar erros e melhorar sua leitura.
+// style50 aula1.c use para melhorar a indentacao.
+// check50 use voce verifica a exatidao do seu codigo com alguns testes automatizados.
+
+/*
+ls = no prompt usamos ls para listar arquivos.
+
+rm = remove arquivos ex: re aula1
+           rm: remove regular file 'aula1'? y
+
+                               neste caso o y (yes) ele aceita o comando poderia se n (not),
+
+mv = renomeia um arquivo,
+mkdir = cria uma pasta (chamamos de diretorio).
+cd = para mudar o diretório.
+mv 'nome pasta' .. = sobe a pasta para o nivel de cima.
+cd .. = muda para o diretorio.
+rmdir 'nome pasta' = remove o diretório(pasta).
+./'nome pasto ou arquivo' = poem o a poarta ou arquivo para baixo acessa o arquivo podendo debugar.
+
+bool                              
+char                              %c    porcentagem sera usado para ocupar um local para ser impresso um caracter. 
+double                            
+float                             %f    para imprimir um numero flutuante 
+int                               %i    para inprimir uminteiro
+long                              %li   para imprimir um long 
+string                            %s    para imprimir uma string
+...
+em C usamos 32 bit oque da uma variacao de 4 bilhoes de opcoes de dados e
+exixte outro tipo de 64 bit quando usamos o double ativamos esta variacao de 64 bit 
+ounde dobramos as opcoes de dados.
+
+operadores 
++
+-
+/
+*
+
+*/
 
 
 }
