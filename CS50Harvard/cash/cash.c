0ff857b29f1c0cad211aa5ecd6b8f3925a9bcefc
Jhonatan de Oliveira <Jhonatandevbr@gmail.com>
2023-03-27 23:11:17 +0900

    .
    .

---
+++ CS50Harvard/cash/cash.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/*
+Use get_float para obter a entrada do usuário e printf para gerar sua resposta.
+Pedimos que você use get_float para que possa lidar com reais e centavos.
+Se o usuário não fornecer um valor não negativo, 
+seu programa deve solicitar novamente ao usuário uma quantia válida 
+até que o usuário concorde.
+Converter os dólares inseridos pelo usuário em centavos 
+ (ou seja, de um float para um int).
+ Utilize o ponto final ao invés de vírgula!!
+ Usando o round, que é declarado na math.h.
+ ...int centavos = round(reais * 100);
+*/
+
+int main(void)
+{
+
+}
