51d705a71fcf5419176886fef1f58eea79f342a5
Jhonatan de Oliveira <jhonatandevbr@gmail.com>
2023-04-28 19:10:04 +0900

    teste com Dom
    Abrindo REACT e testando DOM

---
+++ logica de programaçao com JavaScript/exercicios do livro5/index.js
@@ -0,0 +1,21 @@
+let divReact = document.getElementById('react')
+
+// trocando o const por let para poder realizar mais testes 
+
+console.log(divReact);
+console.log(divReact);
+console.log(divReact);
+
+function testeDom(){
+  divReact = document.getElementById('react').innerHTML="Teste com Dom.";
+}
+
+testeDom();
+// hiiiii haaa bom demais aprender kkkkk.....
+
+function testColor(){
+   divReact = document.getElementById('react').style.color="red";
+}
+
+testColor();
+// acho que estou pegando o jeito
